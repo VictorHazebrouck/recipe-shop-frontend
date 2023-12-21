7b3e05c4d452675f0d0f005c81905588df26b9e4
David <evil.dave59@gmail.com>
2023-12-21 13:07:17 +0100

    css design planningscreen

--- components/Planning.js
+++ components/Planning.js
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Modal } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  Dimensions,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import AgendaScroll from "./AgendaScroll";
@@ -8,6 +15,8 @@ import { modifyCurrentRecipe } from "../reducers/user";
 import ROUTE from "../globals/nico";
 import RecipeModal from "./RecipeModal";
 import { ScreenWidth } from "react-native-elements/dist/helpers";
+
+const screenWidth = Dimensions.get("window").width;
 
 export default function PlanningScreen({ navigation }) {
   const dispatch = useDispatch();
@@ -72,6 +81,7 @@ export default function PlanningScreen({ navigation }) {
 
     return (
       <View style={styles.dayContainer}>
+        <View style={styles.separation}></View>
         <Text style={styles.dayText}>{capitalizedDay}</Text>
       </View>
     );
@@ -104,7 +114,7 @@ export default function PlanningScreen({ navigation }) {
 
   const renderItem = (day, itemData, i) => {
     return (
-      <Card style={styles.card} key={i}>
+      <View style={styles.card} key={i}>
         <Card.Content style={styles.cardContent}>
           <TouchableOpacity onPress={() => handleModal(itemData.id)}>
             <Text style={styles.textCard}>{itemData.id.name}</Text>
@@ -112,16 +122,16 @@ export default function PlanningScreen({ navigation }) {
           <TouchableOpacity onPress={() => handleDeleteRecipe(itemData)}>
             <FontAwesome
               style={{ marginLeft: 10 }}
-              name="trash-o"
+              name="trash"
               size={25}
-              color="#CC3F0C"
+              color="#937B8A"
             />
           </TouchableOpacity>
         </Card.Content>
         <Modal visible={modalVisible} animationType="slide">
           <RecipeModal {...currentRecipe} closeModal={closeModal}></RecipeModal>
         </Modal>
-      </Card>
+      </View>
     );
   };
 
@@ -145,26 +155,28 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: "#CC3F0C",
     borderRadius: 5,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
     alignSelf: "flex-end",
+    marginVertical: 20,
   },
   card: {
     alignSelf: "flex-end",
     marginBottom: 10,
-  },
-  cardContent: {
+    maxWidth: 250,
     paddingVertical: 3,
-    paddingHorizontal: 6,
     borderWidth: 1,
     borderColor: "#937B8A",
     borderRadius: 3,
+  },
+  cardContent: {
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
   },
   textCard: {
     color: "#937B8A",
+    flexWrap: "wrap",
   },
   dayContainer: {
     alignSelf: "flex-start",
@@ -175,5 +187,10 @@ const styles = StyleSheet.create({
     fontFamily: "Anton-reg",
     color: "#4B3B47",
     textAlign: "left",
+  },
+  separation: {
+    height: 1,
+    width: ScreenWidth - 40,
+    backgroundColor: "#4B3B47",
   },
 });
